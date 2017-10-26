42493f5c9091fb6b3e41a1d14060e0df085ed799
Unknown <cltorrento@gmail.com>
2017-10-26 10:31:06 -0600

    Fixing import persistence

--- src/main/java/com/torrento/Prueba/mvcTest/entity/User.java
+++ src/main/java/com/torrento/Prueba/mvcTest/entity/User.java
@@ -1,7 +1,75 @@
 package main.java.com.torrento.Prueba.mvcTest.entity;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
 
+
+@Entity
+@Table(name="users")
 public class User implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="Id")
+	private int Id;
+	
+	@Column(name="Name")
+	private String Name;
+	
+	@Column(name="Surname")
+	private String Surname;
+	
+	@Column(name="User")
+	private String User;
+	
+	@Column(name="Password")
+	private String Password;
+	
+	@Column(name="Email")
+	private String Email;
+	
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getSurname() {
+		return Surname;
+	}
+	public void setSurname(String surname) {
+		Surname = surname;
+	}
+	public String getUser() {
+		return User;
+	}
+	public void setUser(String user) {
+		User = user;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
 }
